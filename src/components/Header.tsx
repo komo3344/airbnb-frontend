17fefab8b45ffd0ce9184fe4e2437a19b0e29758
junyoung <jy940608@naver.com>
2023-04-02 01:56:04 +0900

    style: 그리드 방 하트표시

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,30 +1,64 @@
-import { Box, Button, HStack, IconButton, LightMode, useColorMode, useColorModeValue, useDisclosure} from '@chakra-ui/react';
-import {FaAirbnb, FaMoon, FaSun} from 'react-icons/fa'
-import LoginModal from './LoginModal';
-import SignUpModal from './SignUpModal';
+import {
+  Box,
+  Button,
+  HStack,
+  IconButton,
+  LightMode,
+  Stack,
+  useColorMode,
+  useColorModeValue,
+  useDisclosure,
+} from "@chakra-ui/react";
+import { FaAirbnb, FaMoon, FaSun } from "react-icons/fa";
+import LoginModal from "./LoginModal";
+import SignUpModal from "./SignUpModal";
 
 export default function Header() {
-    const {isOpen: isLoginOpen, onOpen: onLoginOpen, onClose: onLoginClose} = useDisclosure()
-    const {isOpen: isSignUpOpen, onOpen: onSignUpOpen, onClose: onSignUpClose} = useDisclosure()
-    const { toggleColorMode } = useColorMode();
-    const logoColor = useColorModeValue("red.500", "red.300");
-    const Icon = useColorModeValue(FaMoon, FaSun);
-    return (
-        <HStack paddingX={40} paddingY={5} justifyContent={'space-between'} borderBottomWidth={1}>
-            <Box color={logoColor}>
-                <FaAirbnb size={48} />
-            </Box>
-            <HStack spacing={2}>
-                <IconButton onClick={toggleColorMode} variant='ghost' aria-label='다크모드' icon={<Icon />} />
-                <Button onClick={onLoginOpen}>로그인</Button>
-                <LightMode>
-                    <Button onClick={onSignUpOpen} colorScheme={"red"}>
-                        회원가입
-                    </Button>
-                </LightMode>
-            </HStack>
-            <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
-            <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
-        </HStack>
-    )
+  const {
+    isOpen: isLoginOpen,
+    onOpen: onLoginOpen,
+    onClose: onLoginClose,
+  } = useDisclosure();
+  const {
+    isOpen: isSignUpOpen,
+    onOpen: onSignUpOpen,
+    onClose: onSignUpClose,
+  } = useDisclosure();
+  const { toggleColorMode } = useColorMode();
+  const logoColor = useColorModeValue("red.500", "red.300");
+  const Icon = useColorModeValue(FaMoon, FaSun);
+  return (
+    <Stack
+      paddingX={40}
+      paddingY={5}
+      alignItems="center"
+      justifyContent={"space-between"}
+      direction={{
+        sm: "column",
+        md: "row",
+      }}
+      borderBottomWidth={1}
+      spacing={{ sm: 4, md: 0 }}
+    >
+      <Box color={logoColor}>
+        <FaAirbnb size={48} />
+      </Box>
+      <HStack spacing={2}>
+        <IconButton
+          onClick={toggleColorMode}
+          variant="ghost"
+          aria-label="다크모드"
+          icon={<Icon />}
+        />
+        <Button onClick={onLoginOpen}>로그인</Button>
+        <LightMode>
+          <Button onClick={onSignUpOpen} colorScheme={"red"}>
+            회원가입
+          </Button>
+        </LightMode>
+      </HStack>
+      <LoginModal isOpen={isLoginOpen} onClose={onLoginClose} />
+      <SignUpModal isOpen={isSignUpOpen} onClose={onSignUpClose} />
+    </Stack>
+  );
 }
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,34 +1,70 @@
-import { Box, Grid, Text, Image, VStack, HStack } from '@chakra-ui/react';
-import {FaStar} from 'react-icons/fa'
+import {
+  Box,
+  Grid,
+  Text,
+  Image,
+  VStack,
+  HStack,
+  Button,
+} from "@chakra-ui/react";
+import { FaStar, FaRegHeart } from "react-icons/fa";
+import Room from "../components/Room";
 
 export default function Home() {
-    return <Grid px={40} mt={10} columnGap={4} rowGap={8} templateColumns={"repeat(5, 1fr)"}>
-        <VStack alignItems={"flex-start"}>
-            <Box overflow={"hidden"} mb={3} rounded="3xl">
-                <Image
-                    h="280"
-                    src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
-                />
-            </Box>
-            <Box>
-                <Grid gap={2} gridTemplateColumns={'6fr 1fr'}>
-                    <Text display={"block"} as="b" noOfLines={1} fontSize={"md"}>
-                        Ganggu-myeon, Yeongdeok-gun, 경상북도, 한국
-                    </Text>
-                    <HStack spacing={1}>
-                        <FaStar size={15} />
-                        <Text>5.0</Text>
-                    </HStack>
-                </Grid>
-                <Text fontSize={"sm"} color={"gray.600"}>
-                    대한민국 경상북도
-                </Text>
-            </Box>
-            <Text fontSize={"sm"} color={"gray.600"}>
-                <Text as="b">₩286,200</Text> /박
+  return (
+    <Grid
+      px={40}
+      mt={10}
+      columnGap={4}
+      rowGap={8}
+      templateColumns={{
+        sm: "1fr",
+        md: "1fr 1fr",
+        lg: "repeat(3, 1fr)",
+        xl: "repeat(4, 1fr)",
+        "2xl": "repeat(5, 1fr)",
+      }}
+    >
+      {[
+        2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2,
+        2, 2, 2, 2, 2, 2, 2, 2,
+      ].map((index) => (
+        <Room key={index} />
+      ))}
+      <VStack alignItems={"flex-start"}>
+        <Box position={"relative"} overflow={"hidden"} mb={3} rounded="3xl">
+          <Image
+            h="280"
+            src="https://a0.muscache.com/im/pictures/miso/Hosting-47181423/original/39c9d4e7-78d0-4807-9f0d-3029d987d02a.jpeg?im_w=720"
+          />
+          <Button
+            variant={"unstyled"}
+            position={"absolute"}
+            top={0}
+            right={0}
+            color="white"
+          >
+            <FaRegHeart size={"20px"} />
+          </Button>
+        </Box>
+        <Box>
+          <Grid gap={2} gridTemplateColumns={"6fr 1fr"}>
+            <Text display={"block"} as="b" noOfLines={1} fontSize={"md"}>
+              Ganggu-myeon, Yeongdeok-gun, 경상북도, 한국
             </Text>
-
-        </VStack>
-
-    </Grid>;
+            <HStack spacing={1}>
+              <FaStar size={15} />
+              <Text>5.0</Text>
+            </HStack>
+          </Grid>
+          <Text fontSize={"sm"} color={"gray.600"}>
+            대한민국 경상북도
+          </Text>
+        </Box>
+        <Text fontSize={"sm"} color={"gray.600"}>
+          <Text as="b">₩286,200</Text> /박
+        </Text>
+      </VStack>
+    </Grid>
+  );
 }
