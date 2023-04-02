9e27a6afdfd866161694f6c95aad8ce821128bed
junyoung <jy940608@naver.com>
2023-04-03 07:58:07 +0900

    add: axios 적용

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,8 @@
+import axios from "axios";
+
 const BASE_URL = "http://127.0.0.1:8000/api/v1";
 
 export default async function getRooms() {
-  const response = await fetch(`${BASE_URL}/rooms/`);
-  const json = await response.json();
-  return json;
+  const response = await axios.get(`${BASE_URL}/rooms/`);
+  return response.data;
 }
