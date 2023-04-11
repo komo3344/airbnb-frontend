9a3da5f485e38407d122815073f326ee9f96d161
junyoung <jy940608@naver.com>
2023-04-12 08:49:01 +0900

    feat: github social login

--- src/components/SocialLogin.tsx
+++ src/components/SocialLogin.tsx
@@ -1,26 +1,30 @@
-import { Box, Button, Divider, HStack, Text, VStack } from '@chakra-ui/react';
-import {FaGithub, FaComment} from 'react-icons/fa'
+import { Box, Button, Divider, HStack, Text, VStack } from "@chakra-ui/react";
+import { FaGithub, FaComment } from "react-icons/fa";
 
 export default function SocialLogin() {
-    return (
-        <Box mb={4}>
-            <HStack marginY={8}>
-                <Divider />
-                <Text
-                    textTransform={'uppercase'}
-                    color="gray.500"
-                    fontSize="xs"
-                    as="b"
-                >
-                    or
-                </Text>
-                <Divider />
-
-            </HStack>
-            <VStack>
-                <Button w={"100%"} colorScheme={"gray"} leftIcon={<FaGithub />}>Github로 로그인하기</Button>
-                <Button w={"100%"} colorScheme={"yellow"} leftIcon={<FaComment />}>Kakao로 로그인하기</Button>
-            </VStack>
-        </Box>
-    )
+  return (
+    <Box mb={4}>
+      <HStack marginY={8}>
+        <Divider />
+        <Text textTransform={"uppercase"} color="gray.500" fontSize="xs" as="b">
+          or
+        </Text>
+        <Divider />
+      </HStack>
+      <VStack>
+        <Button
+          as="a"
+          href="https://github.com/login/oauth/authorize?client_id=3429bbb417261e7ad92f&scope=read:user,user:email"
+          w={"100%"}
+          colorScheme={"gray"}
+          leftIcon={<FaGithub />}
+        >
+          Github로 로그인하기
+        </Button>
+        <Button w={"100%"} colorScheme={"yellow"} leftIcon={<FaComment />}>
+          Kakao로 로그인하기
+        </Button>
+      </VStack>
+    </Box>
+  );
 }
--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import RoomDetail from "./components/RoomDetail";
 import Root from "./components/Root";
+import GithubConfirm from "./routes/GithubConfirm";
 import Home from "./routes/Home";
 import NotFound from "./routes/NotFound";
 
@@ -17,6 +18,15 @@ const router = createBrowserRouter([
       {
         path: "rooms/:roomPk",
         element: <RoomDetail />,
+      },
+      {
+        path: "social",
+        children: [
+          {
+            path: "github",
+            element: <GithubConfirm />,
+          },
+        ],
       },
     ],
   },
